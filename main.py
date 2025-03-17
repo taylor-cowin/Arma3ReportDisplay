1ca4ed4e0c7dedc5f85fe886dc9b3ef93bd025f0
Taylor Cowin <80193327+taylor-cowin@users.noreply.github.com>
2025-03-16 21:26:36 -0500

    switching PCs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 from pathlib import WindowsPath
+import os
+import threading
+
+current_logfile = None
 
 def get_report_dir():
     home_path = Path.home()
@@ -8,14 +12,33 @@ def get_report_dir():
         if expected_dir.exists():
             return expected_dir
         else:
-            report_error('Could not find ' + str(expected_dir))
+            handle_error('Could not find ' + str(expected_dir))
         
     else:
-        report_error('This program can only be run on Windows machines.')
+        handle_error('This program can only be run on Windows machines.')
     
-def report_error(e):
+def check_for_rpt_file():
+    files = os.listdir(report_dir)
+    
+            #check list of files every 30 seconds
+            #if new file, switch to it
+def print_log_lines():
+        #print last line of file unless it has already been printed - check every 15ms   
+    
+def handle_error(e):
     input('Unable to proceed: ' + str(e))
     quit()
         
 if __name__ == "__main__":
     report_dir = get_report_dir()
+    
+    check_thread = threading.Thread(check_for_rpt_file)
+    parse_thread = threading.Thread(print_log_lines)
+    
+    check_thread.start()    
+    parse_thread.start()
+    
+    check_thread.join()
+    parse_thread.join()
+
+    print('Execution complete. Program will now exit.')
