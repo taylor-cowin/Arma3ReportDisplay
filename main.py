7ef6bd785446010063b81386de8a31914f3169da
TaylorCowin <80193327+taylor-cowin@users.noreply.github.com>
2025-03-17 23:36:13 -0500

    Cleaning up some of the code prior to last touches. Printing still broken and probably needs substantial rewrite

--- main.py
+++ main.py
@@ -11,83 +11,95 @@ import datetime
 current_logfile = None
 file_updated = False
 last_line_printed = None
+report_dir = None
+file_check_rate = 10 #How often (in seconds) to check for a new rpt file
 
-def get_report_dir():
+def set_report_dir():
+    global report_dir
+    
     home_path = Path.home()
     if type(home_path) is WindowsPath:
         expected_dir = WindowsPath(home_path,'AppData','Local','Arma 3')
         if expected_dir.exists():
-            return expected_dir
+            report_dir = expected_dir
         else:
             handle_error('Could not find ' + str(expected_dir))
         
     else:
         handle_error('This program can only be run on Windows machines.')
 
-def set_current_logfile(file):
-    global current_logfile
-    current_logfile = file
-
-def compare_timestamps(old_stamp, new_stamp):
-    old_date = parse_datetime(old_stamp)
-    new_date = parse_datetime(new_stamp)
-    return old_date < new_date #return true if old date is older
-    
-def parse_datetime(stamp):
-    def _get_year(stamp):
-        return int(stamp[0:4])
-    def _get_month(stamp):
-        return int(stamp[5:7])
-    def _get_day(stamp):
-        return int(stamp[8:10])
-    def _get_hour(stamp):
-        return int(stamp[11:13])
-    def _get_minute(stamp):
-        return int(stamp[14:16])
-    def _get_second(stamp):
-        return int(stamp[17:19])
-    return datetime.datetime(_get_year(stamp), _get_month(stamp), _get_day(stamp), _get_hour(stamp), _get_minute(stamp), _get_second(stamp))
-    
-def get_timestamp(file):
-    dirty_timestamp = re.search(r"[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}.rpt", file) #get the end of the filename
-    clean_timestamp = str(dirty_timestamp.group()).split('.')[0] #remove the .rpt
-    return clean_timestamp
-
-def parse_report_timestamp(file):
-    global current_logfile
-    global file_updated
-    if current_logfile is None:
-        current_logfile = file
-        file_updated = True
-        check_for_rpt_file() #Initialized, so start the loop again in case there are more
-    else:
-        old_file = get_timestamp(current_logfile) #inefficient -- should store the timestamp alongside the file to prevent constant matching
-        new_file = get_timestamp(file)
-        #Returns true if file is newer
-        if old_file != new_file:
-            if compare_timestamps(old_file, new_file):
-                current_logfile = file
-                file_updated = True
-
-def check_for_rpt(file):
-    if re.search(r".rpt", file):
-        return True
-    return False
-
 def check_for_rpt_file():
     global file_updated
+    global file_check_rate
+    def _compare_timestamps(old_stamp, new_stamp):
+        old_date = _parse_datetime(old_stamp)
+        new_date = _parse_datetime(new_stamp)
+        return old_date < new_date #return true if old date is older
+
+    def _parse_datetime(stamp):
+        def _get_year(stamp):
+            return int(stamp[0:4])
+        def _get_month(stamp):
+            return int(stamp[5:7])
+        def _get_day(stamp):
+            return int(stamp[8:10])
+        def _get_hour(stamp):
+            return int(stamp[11:13])
+        def _get_minute(stamp):
+            return int(stamp[14:16])
+        def _get_second(stamp):
+            return int(stamp[17:19])
+        return datetime.datetime(_get_year(stamp), _get_month(stamp), _get_day(stamp), _get_hour(stamp), _get_minute(stamp), _get_second(stamp))
+    
+    def _get_timestamp(file):
+        dirty_timestamp = re.search(r"[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}.rpt", file) #get the end of the filename
+        clean_timestamp = str(dirty_timestamp.group()).split('.')[0] #remove the .rpt
+        return clean_timestamp
+
+    def _parse_report_timestamp(file):
+        global current_logfile
+        global file_updated
+
+        #First run
+        if current_logfile is None:
+            _set_current_logfile(file)
+            file_updated = True
+            check_for_rpt_file() #Just set the first file, so start the loop again in case there are more
+        #Subsequent runs
+        else:
+            old_file = _get_timestamp(current_logfile) #inefficient -- should store the timestamp alongside the file to prevent constant matching
+            new_file = _get_timestamp(file)
+            #Returns true if file is newer
+            if old_file != new_file:
+                if _compare_timestamps(old_file, new_file):
+                    _set_current_logfile(file)
+                    file_updated = True
+    
+    def _set_current_logfile(file):
+        global current_logfile
+        current_logfile = file
+
+    def _check_for_rpt_ext(file):
+        if re.search(r".rpt", file):
+            return True
+        return False
+    
     files = os.listdir(report_dir)
     report_list = []
     #Make a list of all reports
     for file in files:
-        if check_for_rpt(file):
+        #Add to list if it is an '*.rpt' file
+        if _check_for_rpt_ext(file):
             report_list.append(file)
+    #If no report files are found
     if report_list == []:
         handle_error("No .rpt files found at " + str(report_dir))
+    #Report files were found, so check timestamp for each to see if it is the most recent
     else:
         for rpt in report_list:
-            parse_report_timestamp(rpt)
-    time.sleep(10)
+            _parse_report_timestamp(rpt)
+    #Check for new log files every 10 seconds
+    time.sleep(file_check_rate)
     check_for_rpt_file()
             
 def print_log_lines():
@@ -116,11 +128,12 @@ def print_log_lines():
         if this_line != last_line_printed:
             print(this_line)
             last_line_printed = this_line
-        
+    #If a log file exists, set the path   
     if current_logfile != None:
-        log_full_path = str(get_report_dir()) + '\\' + current_logfile
+        log_full_path = str(report_dir) + '\\' + current_logfile
         _print_loop()
-    else: #Keep checking until there's a file
+    #If no log file exists, keep checking
+    else:
         time.sleep(.1)
         print_log_lines()
         
@@ -129,7 +142,7 @@ def handle_error(e):
     quit()
         
 if __name__ == '__main__':
-    report_dir = get_report_dir()
+    set_report_dir()
     
     check_thread = threading.Thread(group=None, target=check_for_rpt_file)
     parse_thread = threading.Thread(group=None, target=print_log_lines)
